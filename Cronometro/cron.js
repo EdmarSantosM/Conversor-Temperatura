13507ebe2b640876e5cfc970e9395a8695df9d56
EdmarSantosM <santosem4@hotmail.com>
2020-12-05 23:19:27 -0300

    Update cron.js
    Declaração
    
    Variáveis
    Functions

--- Cronometro/cron.js
+++ Cronometro/cron.js
@@ -1,1 +1,23 @@
 "use strict"
+
+// Declaração  das variáveis
+
+var hh = 0;
+var mm = 0;
+var ss = 0;
+
+ // Funções
+
+ function start(){
+
+ }
+
+ 
+ function pause(){
+     
+}
+
+
+function stop(){
+     
+}
