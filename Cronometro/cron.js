687c08a9e2bfca01dd1adde6eee14d9bffe948f3
EdmarSantosM <santosem4@hotmail.com>
2020-12-05 23:47:15 -0300

    Update cron.js
    Aplicando  as condicionais

--- Cronometro/cron.js
+++ Cronometro/cron.js
@@ -31,6 +31,19 @@ function stop() {
 }
 
 function timer(){
-    var format = hh + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0 + ss : ss');   // Instrução ternária
+
+   ss++;
+   if(ss == 60){
+       ss = 0;
+       mm++;
+
+       if(mm == 60){
+           mm = 0;
+           hh++;
+       }
+   }
+
+
+    var format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);  // INstrução ternaria
     document.getElementById('counter').innerHTML = format;
 }
